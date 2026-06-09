356fcd8efcb14c552246feddaadbc9be1350d73c
Heritier Moise <vitalheritier1@gmail.com>
2026-06-09 13:31:33 +0300

    Add Vercel configuration and improve API routing

--- api/[...slug].js
+++ api/[...slug].js
@@ -1,3 +1,5 @@
 import app from '../backend/index.js'
 
+// Vercel fait passer les requêtes directement à Express
+// Express va gérer tous les routes: /api/*, les fichiers statiques, et le fallback SPA
 export default app
