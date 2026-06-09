77105ae7db28b811b34f342ff2f00a0d35c0dea2
Heritier Moise <vitalheritier1@gmail.com>
2026-06-09 14:24:10 +0300

    Remove accidental catchall handler

--- api/[...catchall].js
+++
@@ -1,7 +0,0 @@
-import app from '../backend/index.js'
-
-// Ce fichier gère toutes les routes non-/api
-// (les routes /api/* sont gérées par [...slug].js)
-// Les routes SPA comme /inscription sont routées ici par Vercel
-// Express servira index.html qui laissera React Router prendre le contrôle
-export default app
